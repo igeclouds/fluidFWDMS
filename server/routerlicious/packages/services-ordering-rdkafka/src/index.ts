81e09b1f8e0c493a1c26749e827a0f279a5cc224
Andrew Stanovsky <astanovsky@microsoft.com>
2023-01-18 14:44:04 -0800

    adding segfault dump module

--- server/routerlicious/packages/services-ordering-rdkafka/src/index.ts
+++ server/routerlicious/packages/services-ordering-rdkafka/src/index.ts
@@ -3,6 +3,9 @@
  * Licensed under the MIT License.
  */
 
+var SegfaultHandler = require('segfault-handler');
+SegfaultHandler.registerHandler("crash.log"); // With no argument, SegfaultHandler will generate a generic log file name
+
 export { IKafkaConsumerOptions, RdkafkaConsumer } from "./rdkafkaConsumer";
 export { IKafkaProducerOptions, RdkafkaProducer } from "./rdkafkaProducer";
 export { IRdkafkaResources, RdkafkaResources, RdkafkaResourcesFactory } from "./resourcesFactory";
